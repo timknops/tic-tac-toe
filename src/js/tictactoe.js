60d054ee29d26264e1c31a7f21e7542584b6175f
Tim Knops <timknops22@gmail.com>
2023-08-26 13:15:50 +0200

    Setting up js structure and tailwindcss

---
+++ src/js/tictactoe.js
@@ -0,0 +1,13 @@
+const gameBoard = (() => {
+  const board = ["X", "O", "X", "X", "O", "O", "X", "O", "X"];
+
+  return { board };
+})();
+
+const Player = (symbol) => {
+  return { symbol };
+};
+
+const gameController = (() => {})();
+
+const displayController = (() => {})();
