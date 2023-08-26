5821c5edff26429cb2a400bfa354d3e434038d2d
Tim Knops <timknops22@gmail.com>
2023-08-26 20:33:15 +0200

    Adding the icon displaying on click functionality

--- src/js/tictactoe.js
+++ src/js/tictactoe.js
@@ -1,13 +1,52 @@
 const gameBoard = (() => {
-  const board = ["X", "O", "X", "X", "O", "O", "X", "O", "X"];
+  const board = new Array(9);
 
   return { board };
 })();
 
 const Player = (symbol) => {
-  return { symbol };
+  const getSymbol = () => {
+    return symbol;
+  };
+
+  return { getSymbol };
 };
 
-const gameController = (() => {})();
+const gameController = (() => {
+  const player1 = Player("X");
+  const player2 = Player("O");
+  let currentRound = 1;
+  let currentPlayerSymbol = player1.getSymbol();
 
-const displayController = (() => {})();
+  return {
+    currentRound,
+    currentPlayerSymbol,
+  };
+})();
+
+const displayController = (() => {
+  const boardFields = document.querySelectorAll(".board-field");
+  const playerColors = ["text-emerald-500", "text-amber-500"];
+
+  boardFields.forEach((field) => {
+    field.addEventListener("click", () => {
+      if (!field.hasChildNodes()) {
+        field.appendChild(createPlayerIcon());
+      }
+    });
+  });
+
+  const createPlayerIcon = () => {
+    const node = document.createElement("i");
+    node.classList.add(
+      "fa-solid",
+      `fa-${gameController.currentPlayerSymbol.toLowerCase()}`,
+      "fa-10x",
+      gameController.currentPlayerSymbol === "X"
+        ? playerColors[0]
+        : playerColors[1]
+    );
+
+    return node;
+  };
+})();
