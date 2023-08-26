85a429b456993782af9c1d79603b143dfad43954
Tim Knops <timknops22@gmail.com>
2023-08-26 14:47:25 +0200

    Working on general styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/*.html", "./src/js/*.js"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Lato", "sans-serif"],
+    },
   },
   plugins: [],
 };
